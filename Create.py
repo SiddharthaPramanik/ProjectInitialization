d96276798c67fbbd8ac34eaf3c8fe349d083a4f4
Siddhartha Pramanik <rks.siddhartha@gmail.com>
2019-08-25 01:55:38 +0530

    New repository creation

--- Create.py
+++ Create.py
@@ -1,17 +1,36 @@
+import sys
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
-username = 'rks.siddhartha@gmail.com'
-password = '#Writer248625'
+def create():
+    if len(sys.argv) > 3:
+        print('Only 2 arguments are expected! Please check if the description is in quotes.')
 
-driver = webdriver.Firefox(executable_path='./geckodriver')
-driver.get('https://github.com/login')
-login_field = driver.find_element_by_id('login_field')
-login_field.clear()
-login_field.send_keys(username)
-pass_field = driver.find_element_by_id('password')
-pass_field.clear()
-pass_field.send_keys(password)
-submit = driver.find_element_by_name('commit')
-submit.click()
-# driver.quit()
+    else:
+        username = 'rks.siddhartha@gmail.com'
+        password = '#Writer248625'
+
+        driver = webdriver.Firefox(executable_path='./geckodriver')
+
+        def fill_text_field(id, keys):
+            text_field = driver.find_element_by_id(id)
+            text_field.clear()
+            text_field.send_keys(keys)
+
+        # Login Automation
+        driver.get('https://github.com/login')
+        fill_text_field('login_field', username)
+        fill_text_field('password', password)
+        button = driver.find_element_by_name('commit')
+        button.click()
+
+        # Create new repository
+        driver.get('https://github.com/new')
+        fill_text_field('repository_name', sys.argv[1])
+        if len(sys.argv) == 2:
+            fill_text_field('repository_description', sys.argv[2])
+        button = driver.find_element_by_class_name('first-in-line')
+        button.submit()
+        driver.quit()
+
+if __name__ == '__main__':
+    create()
